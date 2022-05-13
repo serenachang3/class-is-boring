4eb0f6720e0dafc5b9876a0a3c976513b2851e54
Serena Chang <serena.w.chang@gmail.com>
2022-05-13 19:28:40 -0400

    useState

--- src/exercises/02.js
+++ src/exercises/02.js
@@ -1,26 +1,59 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 
 class ClassComp02 extends Component {
-    constructor() {
-        super();
-        this.state = { isBored: false };
-    }
-    render() {
-        const { isBored } = this.state;
-        return (
-            <div>
-                <h1>{isBored ? "Yeah, I'm bored too." : "Really? Well good for you I suppose."}</h1>
-                <div>
-                    {!isBored && <button onClick={() => this.setState({ isBored: true })}>I'm Bored</button>}
-                    {isBored && <button onClick={() => this.setState({ isBored: false })}>I'm Not Bored</button>}
-                </div>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = { isBored: false };
+  }
+  render() {
+    const { isBored } = this.state;
+    return (
+      <div>
+        <h1>
+          {isBored
+            ? "Yeah, I'm bored too."
+            : "Really? Well good for you I suppose."}
+        </h1>
+        <div>
+          {!isBored && (
+            <button onClick={() => this.setState({ isBored: true })}>
+              I'm Bored
+            </button>
+          )}
+          {isBored && (
+            <button onClick={() => this.setState({ isBored: false })}>
+              I'm Not Bored
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 const FunctionalComponent02 = () => {
-    return null;
+  const [isBored, setIsBored] = useState(false);
+  return (
+    <div>
+      <h1>
+        {isBored
+          ? "Yeah, I'm bored too."
+          : "Really? Well good for you I suppose."}
+      </h1>
+      <div>
+        {!isBored && (
+          <button onClick={() => setIsBored(true)}>
+            I'm Bored
+          </button>
+        )}
+        {isBored && (
+          <button onClick={() => setIsBored(false)}>
+            I'm Not Bored
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
-export default ClassComp02;
+export default FunctionalComponent02;
