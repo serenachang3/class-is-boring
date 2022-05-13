2e5f48fc863266db325258171b046388beda1239
Serena Chang <serena.w.chang@gmail.com>
2022-05-13 19:58:08 -0400

    useEffect

--- src/exercises/03.js
+++ src/exercises/03.js
@@ -1,29 +1,46 @@
-import { Component } from "react";
+import { Component, useEffect, useState } from "react";
 
-import { oneRandomActivity } from '../bored-api';
+import { oneRandomActivity } from "../bored-api";
 
 class ClassComp03 extends Component {
-    constructor() {
-        super();
-        this.state = {
-            doWhenImBored: null,
-        };
+  constructor() {
+    super();
+    this.state = {
+      doWhenImBored: null,
+    };
+  }
+  async componentDidMount() {
+    const anActivity = await oneRandomActivity();
+    this.setState({ doWhenImBored: anActivity });
+  }
+  render() {
+    if (this.state.doWhenImBored === null) {
+      return <h2>Siiiiiiigh. Loading ...</h2>;
+    } else {
+      return <h1>{this.state.doWhenImBored}</h1>;
     }
-    async componentDidMount() {
-        const anActivity = await oneRandomActivity();
-        this.setState({ doWhenImBored: anActivity });
-    }
-    render() {
-        if (this.state.doWhenImBored === null) {
-            return <h2>Siiiiiiigh. Loading ...</h2>;
-        } else {
-            return <h1>{this.state.doWhenImBored}</h1>;
-        }
-    }
+  }
 }
 
 const FunctionalComponent03 = () => {
-    return null;
+  const [doWhenImBored, setDoWhenImBored] = useState(null);
+
+  // componentDidMount -- function + empty array
+  // can't make the function you provide to useEffect an async function!
+  useEffect(() => {
+    const run = async () => {
+        const anActivity = await oneRandomActivity();
+        setDoWhenImBored(anActivity)
+    }
+    run();
+    // empty dependency array makes it just render once
+  }, []);
+
+  if (doWhenImBored === null) {
+    return <h2>Siiiiiiigh. Loading ...</h2>;
+  } else {
+    return <h1>{doWhenImBored}</h1>;
+  }
 };
 
-export default ClassComp03;
+export default FunctionalComponent03;
